96c06b75130825a500af7b34821e02dda4134a8b
Rapeapach Suwasri <kagaminelenkyun@gmail.com>
2014-08-23 17:53:58 +0700

    after

--- print_format_type.c
+++ print_format_type.c
@@ -3,13 +3,13 @@
 
 int main() {
 	// type your code here
-	printf("%e\n", 1668);
-	printf("%d\n", 1668.258);
-	printf("%c\n", 1668.258);
-	printf("%.3f\n", "1668.258");
-	printf("%d\n", 422);
-	printf("%d\n", 422);
-	printf("%d\n", M_PI);	// ¤èÒ¤§·Õè M_PI = 3.141593...
+	printf("%d\n", 1668);
+	printf("%.2f\n", 1668.258);
+	printf("%.3f\n", 1668.258);
+	printf("%s\n", "1668.258");
+	printf("%+d\n", 422);
+	printf("%04d\n", 422);
+	printf("%.2f\n", M_PI);	// ¤èÒ¤§·Õè M_PI = 3.141593...
 	printf("%s\n", "I\'m eager to go to the next page");
 	
 
