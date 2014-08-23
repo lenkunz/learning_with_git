c6962daca472073e70ca1f7e8b881846f74dfd42
Rapeapach Suwasri <kagaminelenkyun@gmail.com>
2014-08-23 17:52:38 +0700

    before

---
+++ print_format_type.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	// type your code here
+	printf("%e\n", 1668);
+	printf("%d\n", 1668.258);
+	printf("%c\n", 1668.258);
+	printf("%.3f\n", "1668.258");
+	printf("%d\n", 422);
+	printf("%d\n", 422);
+	printf("%d\n", M_PI);	// ค่าคงที่ M_PI = 3.141593...
+	printf("%s\n", "I\'m eager to go to the next page");
+	
+
+	return 0;
+}
