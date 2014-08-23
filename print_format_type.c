71c2e421c4a092eb2ec4a078acae2b7a11e6f1a0
Rapeapach Suwasri <kagaminelenkyun@gmail.com>
2014-08-23 17:57:18 +0700

    after

--- print_format_type.c
+++ print_format_type.c
@@ -3,19 +3,19 @@
 
 int main() {
 	// type your code here
-	printf("%e\n", 1668);
+	printf("%d\n", 1668);
 	
-	printf("%d\n", 1668.258);
+	printf("%.2f\n", 1668.258);
 
-	printf("%c\n", 1668.258);
+	printf("%.3f\n", 1668.258);
 
 	printf("%.3f\n", "1668.258");
 
-	printf("%d\n", 422);
+	printf("%+d\n", 422);
 
-	printf("%d\n", 422);
+	printf("%04d\n", 422);
 
-	printf("%d\n", M_PI);	// ค่าคงที่ M_PI = 3.141593...
+	printf("%.2f\n", M_PI);	// ค่าคงที่ M_PI = 3.141593...
 
 	printf("%s\n", "I\'m eager to go to the next page");
 	
